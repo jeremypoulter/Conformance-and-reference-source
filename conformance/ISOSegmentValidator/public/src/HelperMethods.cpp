b134e9c4e2f86fa2e6cab9ab67f25989e7925283
Jeremy Poulter <jeremy@bigjungle.net>
2014-10-28 14:53:01 +0000

    Fixes to build with base VS2010 install (platform v1.00)

--- conformance/ISOSegmentValidator/public/src/HelperMethods.cpp
+++ conformance/ISOSegmentValidator/public/src/HelperMethods.cpp
@@ -189,6 +189,20 @@ int getSegmentNumberByOffset(UInt64 offset)
         
 }
 
+long double roundl(long double x) 
+{
+  long double t;
+  if (x >= 0.0) {
+    t = ceill(x);
+    if ((t - x) > 0.5) t -= 1.0;
+    return t;
+  } else {
+    t = ceill(-x);
+    if ((t + x) > 0.5) t -= 1.0;
+    return -t;
+  }
+}
+
 void logtempInfo(MovieInfoRec *mir)
 {
     FILE *leafInfoFile = fopen("sidxinfo.txt","wt");
